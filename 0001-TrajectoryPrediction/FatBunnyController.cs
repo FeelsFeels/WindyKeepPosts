ea74de61dd3b6e05be79834c7f559ba9b8ddb6ff
Fouad Tabsh <tabshf@gmail.com>
2018-01-26 19:05:49 +0200

    VS formatting.

--- 0001-TrajectoryPrediction/FatBunnyController.cs
+++ 0001-TrajectoryPrediction/FatBunnyController.cs
@@ -1,46 +1,53 @@
 ﻿using UnityEngine;
 
-public class FatBunnyController : MonoBehaviour {
+public class FatBunnyController : MonoBehaviour
+{
+    private readonly Vector2 LAUNCH_VELOCITY = new Vector2(20f, 80f);
+    private readonly Vector2 INITIAL_POSITION = Vector2.zero;
+    private readonly Vector2 GRAVITY = new Vector2(0f, -240f);
+    private const float DELAY_UNTIL_LAUNCH = 1f;
+    private int NUM_DOTS_TO_SHOW = 30;
+    private float DOT_TIME_STEP = 0.05f;
 
-	private readonly Vector2 LAUNCH_VELOCITY = new Vector2(20f, 80f);
-	private readonly Vector2 INITIAL_POSITION = Vector2.zero;
-	private readonly Vector2 GRAVITY = new Vector2(0f, -240f);
-	private const float DELAY_UNTIL_LAUNCH = 1f;
-	private int NUM_DOTS_TO_SHOW = 30;
-	private float DOT_TIME_STEP = 0.05f;
+    private bool launched = false;
+    private float timeUntilLaunch = DELAY_UNTIL_LAUNCH;
+    private Rigidbody2D rigidBody;
 
-	private bool launched = false;
-	private float timeUntilLaunch = DELAY_UNTIL_LAUNCH;
-	private Rigidbody2D rigidBody;
+    public GameObject trajectoryDotPrefab;
 
-	public GameObject trajectoryDotPrefab;
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
 
-	private void Awake(){
-		rigidBody = GetComponent<Rigidbody2D> ();
-	}
+    private void Start()
+    {
+        for (int i = 0; i < NUM_DOTS_TO_SHOW; i++)
+        {
+            GameObject trajectoryDot = Instantiate(trajectoryDotPrefab);
+            trajectoryDot.transform.position = CalculatePosition(DOT_TIME_STEP * i);
+        }
+    }
 
-	private void Start(){
-		for (int i = 0; i < NUM_DOTS_TO_SHOW; i++) {
-			GameObject trajectoryDot = Instantiate (trajectoryDotPrefab);
-			trajectoryDot.transform.position = CalculatePosition (DOT_TIME_STEP * i);
-		}
-	}
+    private void Update()
+    {
+        timeUntilLaunch -= Time.deltaTime;
 
-	private void Update () {
-		timeUntilLaunch -= Time.deltaTime;
+        if (!launched && timeUntilLaunch <= 0)
+        {
+            Launch();
+        }
+    }
 
-		if (!launched && timeUntilLaunch <= 0) {
-			Launch ();
-		}
-	}
+    private void Launch()
+    {
+        rigidBody.velocity = LAUNCH_VELOCITY;
 
-	private void Launch(){
-		rigidBody.velocity = LAUNCH_VELOCITY;
+        launched = true;
+    }
 
-		launched = true;
-	}
-
-	private Vector2 CalculatePosition(float elapsedTime){
-		return GRAVITY * elapsedTime * elapsedTime * 0.5f + LAUNCH_VELOCITY * elapsedTime + INITIAL_POSITION;
-	}
+    private Vector2 CalculatePosition(float elapsedTime)
+    {
+        return GRAVITY * elapsedTime * elapsedTime * 0.5f + LAUNCH_VELOCITY * elapsedTime + INITIAL_POSITION;
+    }
 }
